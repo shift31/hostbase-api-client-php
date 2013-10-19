104c127a55c3e7ae16234d14339678cb0453dde6
Ben Getsug <ben@shift31.com>
2013-10-19 03:18:54 -0500

    initial commit

---
+++ src/Shift31/HostbaseClient.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Shift31;
+
+use Httpful\Request;
+
+class HostbaseClient {
+
+
+    protected $uri;
+
+    protected $username;
+
+    protected $password;
+
+
+    /**
+     * @param string $baseUrl
+     * @param string|null $username
+     * @param string|null $password
+     */
+    public function __construct($baseUrl, $username = null, $password = null)
+    {
+        $this->uri = "$baseUrl/hosts";
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+
+    /**
+     * @param string $query
+     * @return mixed
+     * @throws \Exception
+     */
+    public function search($query)
+    {
+        $response = Request::get("{$this->uri}?q=" . urlencode($query))->authenticateWith($this->username, $this->password)->send();
+
+        if ($response->hasErrors()) {
+            throw new \Exception($response);
+        } else {
+            return $response->body;
+        }
+    }
+
+
+	/**
+	 * @param string|null $fqdn
+	 *
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function show($fqdn = null)
+	{
+		$uri = $this->uri;
+
+		if ($fqdn != null) {
+			$uri .= "/$fqdn";
+		}
+
+		$response = Request::get($uri)->send();
+
+		if ($response->hasErrors()) {
+			throw new \Exception($response);
+		} else {
+			return (array) $response->body;
+		}
+	}
+
+
+	/**
+	 * @param array $data
+	 *
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function store(array $data)
+    {
+	    $response = Request::post($this->uri)
+		    ->authenticateWith($this->username, $this->password)
+	        ->body(json_encode($data))
+	        ->sendsType('application/json')
+		    ->send();
+
+	    if ($response->hasErrors()) {
+		    throw new \Exception($response);
+	    } else {
+		    return (array) $response->body;
+	    }
+    }
+
+
+	/**
+	 * @param string $fqdn
+	 * @param array $data
+	 *
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function update($fqdn, array $data)
+	{
+		$response = Request::put("{$this->uri}/$fqdn")
+			->body(json_encode($data))
+			->sendsType('application/json')
+			->send();
+
+		if ($response->hasErrors()) {
+			throw new \Exception($response);
+		} else {
+			return (array) $response->body;
+		}
+	}
+
+
+	/**
+	 * @param $fqdn
+	 *
+	 * @return mixed
+	 * @throws \Exception
+	 */
+	public function destroy($fqdn)
+	{
+		$response = Request::delete("{$this->uri}/$fqdn")->send();
+
+		if ($response->hasErrors()) {
+			throw new \Exception($response);
+		} else {
+			return true;
+		}
+	}
+}

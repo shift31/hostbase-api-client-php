a04897493d6a6eaf211f0627a011a82ca1a1b3f3
Ben Getsug <ben@shift31.com>
2014-03-12 03:21:04 -0500

    adding support for data filtering

--- src/Shift31/HostbaseClient.php
+++ src/Shift31/HostbaseClient.php
@@ -8,233 +8,255 @@ use Httpful\Mime;
 use Httpful\Request;
 use Httpful\Response;
 
+/**
+ * Class HostbaseClient
+ * @package Shift31
+ *
+ * @todo switch to using Guzzle
+ */
 class HostbaseClient
 {
-	/**
-	 * @var string
-	 */
-	protected $baseUrl;
+    /**
+     * @var string
+     */
+    protected $baseUrl;
 
-	/**
-	 * @var string
-	 */
-	protected $resource;
+    /**
+     * @var string
+     */
+    protected $resource;
 
-	/**
-	 * @var string
-	 */
-	protected $uri;
+    /**
+     * @var string
+     */
+    protected $uri;
 
-	/**
-	 * @var null|string
-	 */
-	protected $username;
+    /**
+     * @var null|string
+     */
+    protected $username;
 
-	/**
-	 * @var null|string
-	 */
-	protected $password;
-
-
-	/**
-	 * @param string      $baseUrl
-	 * @param string      $resource
-	 * @param string|null $username
-	 * @param string|null $password
-	 */
-	public function __construct($baseUrl, $resource = 'hosts', $username = null, $password = null)
-	{
-		$this->baseUrl = $baseUrl;
-		$this->resource = $resource;
-		$this->setUri();
-		$this->username = $username;
-		$this->password = $password;
-	}
+    /**
+     * @var null|string
+     */
+    protected $password;
 
 
-	protected function setUri()
-	{
-		$this->uri = "{$this->baseUrl}/{$this->resource}";
-	}
+    /**
+     * @param string $baseUrl
+     * @param string $resource
+     * @param string|null $username
+     * @param string|null $password
+     */
+    public function __construct($baseUrl, $resource = 'hosts', $username = null, $password = null)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->resource = $resource;
+        $this->setUri();
+        $this->username = $username;
+        $this->password = $password;
+    }
 
 
-	/**
-	 * @param $resource
-	 *
-	 * @return HostbaseClient $this
-	 */
-	public function setResource($resource)
-	{
-		$this->resource = $resource;
-		$this->setUri();
-
-		return $this;
-	}
+    protected function setUri()
+    {
+        $this->uri = "{$this->baseUrl}/{$this->resource}";
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getResource()
-	{
-		return $this->resource;
-	}
+    /**
+     * @param $resource
+     *
+     * @return HostbaseClient $this
+     */
+    public function setResource($resource)
+    {
+        $this->resource = $resource;
+        $this->setUri();
+
+        return $this;
+    }
 
 
-	/**
-	 * @return $this
-	 */
-	public function decodeJsonAsArray()
-	{
-		// Example overriding the default JSON handler with one that encodes the response as an array
-		Httpful::register(Mime::JSON, new JsonHandler(array('decode_as_array' => true)));
-
-		return $this;
-	}
+    /**
+     * @return string
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
 
 
-	/**
-	 * @param string $query
-	 * @param int    $limit
-	 * @param bool   $showData
-	 *
-	 * @throws \Exception
-	 * @return array
-	 */
-	public function search($query, $limit = 10000, $showData = true)
-	{
-		$showData = $showData === true ? 1 : 0;
+    /**
+     * @return $this
+     */
+    public function decodeJsonAsArray()
+    {
+        // Example overriding the default JSON handler with one that encodes the response as an array
+        Httpful::register(Mime::JSON, new JsonHandler(array('decode_as_array' => true)));
 
-		$response = Request::get("{$this->uri}?q=" . urlencode($query) . "&size=$limit" . "&showData=$showData")
-			->authenticateWith($this->username, $this->password
-		)->send();
-
-		return $this->processResponse($response);
-	}
+        return $this;
+    }
 
 
-	/**
-	 * @param string|null $id
-	 *
-	 * @return \stdClass
-	 * @throws \Exception
-	 */
-	public function show($id = null)
-	{
-		$uri = $this->uri;
+    /**
+     * @param string $query
+     * @param int $limit
+     * @param bool $showData
+     *
+     * @param null $include
+     * @param null $exclude
+     * @return array
+     */
+    public function search($query, $limit = 10000, $showData = true, $include = null, $exclude = null)
+    {
+        $showData = $showData === true ? 1 : 0;
 
-		if ($id != null) {
-			$this->mangleCidrNotation($id);
-			$uri .= "/$id";
-		}
+        $uri = "{$this->uri}?q=" . urlencode($query) . "&size=$limit" . "&showData=$showData";
 
-		$response = Request::get($uri)->authenticateWith($this->username, $this->password)->send();
+        if ($include) {
+            $uri .= "&include=$include";
+        } elseif($exclude) {
+            $uri .= "&exclude=$exclude";
+        }
 
-		return $this->processResponse($response);
-	}
+        $response = Request::get($uri)
+            ->authenticateWith($this->username, $this->password
+            )->send();
 
-
-	/**
-	 * @param mixed $data
-	 *
-	 * @return \stdClass
-	 * @throws \Exception
-	 */
-	public function store($data)
-	{
-		$response = Request::post($this->uri)
-			->authenticateWith($this->username, $this->password)
-			->body(json_encode($data))
-			->sendsType('application/json')
-			->send();
-
-		return $this->processResponse($response);
-	}
+        return $this->processResponse($response);
+    }
 
 
-	/**
-	 * @param string $id
-	 * @param mixed  $data
-	 *
-	 * @return \stdClass
-	 * @throws \Exception
-	 */
-	public function update($id, $data)
-	{
-		$this->mangleCidrNotation($id);
+    /**
+     * @param string|null $id
+     *
+     * @param null $include
+     * @param null $exclude
+     * @return \stdClass
+     */
+    public function show($id = null, $include = null, $exclude = null)
+    {
+        $uri = $this->uri;
 
-		$response = Request::put("{$this->uri}/$id")
-			->authenticateWith($this->username, $this->password)
-			->body(json_encode($data))
-			->sendsType('application/json')
-			->send();
+        if ($id != null) {
+            $this->mangleCidrNotation($id);
+            $uri .= "/$id";
+        }
 
-		return $this->processResponse($response);
-	}
+        if ($include) {
+            $uri .= "?include=$include";
+        } elseif($exclude) {
+            $uri .= "?exclude=$exclude";
+        }
 
+        $response = Request::get($uri)->authenticateWith($this->username, $this->password)->send();
 
-	/**
-	 * @param string $id
-	 *
-	 * @return bool
-	 * @throws \Exception
-	 */
-	public function destroy($id)
-	{
-		$this->mangleCidrNotation($id);
-
-		$response = Request::delete("{$this->uri}/$id")->authenticateWith(
-			$this->username, $this->password
-		)->send();
-
-		if ($response instanceof Response && $response->hasErrors()) {
-			throw new \Exception($this->getErrorMessage($response));
-		} else {
-			return true;
-		}
-	}
+        return $this->processResponse($response);
+    }
 
 
-	/**
-	 * @param Response $response
-	 *
-	 * @return mixed
-	 * @throws \Exception
-	 */
-	protected function processResponse(Response $response)
-	{
-		if ($response->hasErrors()) {
-			throw new \Exception($this->getErrorMessage($response));
-		}
+    /**
+     * @param mixed $data
+     *
+     * @return \stdClass
+     * @throws \Exception
+     */
+    public function store($data)
+    {
+        $response = Request::post($this->uri)
+            ->authenticateWith($this->username, $this->password)
+            ->body(json_encode($data))
+            ->sendsType('application/json')
+            ->send();
 
-		return is_array($response->body) ? $response->body['data'] : $response->body->data;
-	}
-
-
-	/**
-	 * @param Response $response
-	 *
-	 * @return mixed
-	 */
-	protected function getErrorMessage(Response $response)
-	{
-		if ($response->hasBody()) {
-			if (is_array($response->body)) {
-				return isset($response->body['error']['message']) ? $response->body['error']['message'] : $response->raw_body;
-			} else {
-				return isset($response->body->error->message) ? $response->body->error->message : $response->raw_body;
-			}
-		} else {
-			return $response->raw_body;
-		}
-	}
+        return $this->processResponse($response);
+    }
 
 
-	/**
-	 * @param $id
-	 */
-	protected function mangleCidrNotation(&$id)
-	{
-		if ($this->getResource() == 'subnets') $id = str_replace('/', '_', $id);
-	}
+    /**
+     * @param string $id
+     * @param mixed $data
+     *
+     * @return \stdClass
+     * @throws \Exception
+     */
+    public function update($id, $data)
+    {
+        $this->mangleCidrNotation($id);
+
+        $response = Request::put("{$this->uri}/$id")
+            ->authenticateWith($this->username, $this->password)
+            ->body(json_encode($data))
+            ->sendsType('application/json')
+            ->send();
+
+        return $this->processResponse($response);
+    }
+
+
+    /**
+     * @param string $id
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function destroy($id)
+    {
+        $this->mangleCidrNotation($id);
+
+        $response = Request::delete("{$this->uri}/$id")->authenticateWith(
+            $this->username, $this->password
+        )->send();
+
+        if ($response instanceof Response && $response->hasErrors()) {
+            throw new \Exception($this->getErrorMessage($response));
+        } else {
+            return true;
+        }
+    }
+
+
+    /**
+     * @param Response $response
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function processResponse(Response $response)
+    {
+        if ($response->hasErrors()) {
+            throw new \Exception($this->getErrorMessage($response));
+        }
+
+        return is_array($response->body) ? $response->body['data'] : $response->body->data;
+    }
+
+
+    /**
+     * @param Response $response
+     *
+     * @return mixed
+     */
+    protected function getErrorMessage(Response $response)
+    {
+        if ($response->hasBody()) {
+            if (is_array($response->body)) {
+                return isset($response->body['error']['message']) ? $response->body['error']['message'] : $response->raw_body;
+            } else {
+                return isset($response->body->error->message) ? $response->body->error->message : $response->raw_body;
+            }
+        } else {
+            return $response->raw_body;
+        }
+    }
+
+
+    /**
+     * @param $id
+     */
+    protected function mangleCidrNotation(&$id)
+    {
+        if ($this->getResource() == 'subnets') $id = str_replace('/', '_', $id);
+    }
 }
